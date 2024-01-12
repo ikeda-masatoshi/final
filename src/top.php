fd1ed0fdcce7243f631dd1bca6b28494b416fda8
ikeda-masatoshi <2201352@s.asojuku.ac.jp>
2024-01-12 11:47:50 +0900

    コミット

---
+++ src/top.php
@@ -0,0 +1,51 @@
+<?php
+    const SERVER = 'mysql220.phy.lolipop.lan';
+    const DBNAME = 'LAA1517362-final';
+    const USER = 'LAA1517362';
+    const PASS = 'Pass1121';
+
+    $connect = 'mysql:host='. SERVER . ';dbname='. DBNAME . ';charset=utf8';
+?>
+<!DOCTYPE html>
+<html lang="ja">
+	<head>
+    <meta http-equiv="Cache-Control" content="no-cache">
+		<meta charset="UTF-8">
+		<title>一覧</title>
+	</head>
+	<body>
+        <h1>おすすめの曲一覧</h1>
+        <hr>
+        <button onclick="location.href='ren6-5-input.php'">登録</button>
+        <table>
+    <tr><th>曲番号</th><th>曲名</th><th>アーティスト名</th></tr>
+<?php
+    $pdo=new PDO($connect, USER, PASS);
+    foreach ($pdo->query('select * from product') as $row) {
+        echo '<tr>';
+        echo '<td>', $row['music_id'], '</td>';
+        echo '<td>', $row['music_name'], '</td>';
+        echo '<td>', $row['artist_name'], '</td>';
+        echo '<td>';
+
+        echo '<form action="ren6-8-edit.php" method="post">';
+        echo '<input type="hidden" name="id" value="', $row['id'], '">';
+        echo '<button type="submit">更新</button>';
+        echo '</form>';
+
+        echo '</td>';
+        echo '<td>';
+
+        echo '<form action="ren6-8-delete.php" method="post">';
+        echo '<input type="hidden" name="id" value="', $row['id'], '">';
+        echo '<button type="submit">削除</button>';
+        echo '</form>';
+
+        echo '</td>';
+        echo '</tr>';
+        echo "\n";
+    }
+?>
+    </table>
+    </body>
+</html>
