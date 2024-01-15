8ca7302aee470d7e87b9c1d038789d54eecbf814
ikeda-masatoshi <2201352@s.asojuku.ac.jp>
2024-01-15 10:19:49 +0900

    変更

--- src/toroku-output.php
+++ src/toroku-output.php
@@ -34,6 +34,7 @@
                 echo '<tr>';
                 echo '<td>',$row['music_id'], '</td>';
                 echo '<td>',$row['music_name'], '</td>';
+                echo '<p>':'</p>';
                 echo '<td>',$row['artist_name'], '</td>';
                 echo '</tr>';
                 echo "\n";
